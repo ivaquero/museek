9049ec36f27c2486967cca344d0af2ac3a12c962
ivaquero <msaintms@outlook.com>
2025-04-05 22:05:11 +0800

    add cli

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/ivaquero/museek
+
+go 1.24.1
+
+require (
+	github.com/spf13/cobra v1.9.1
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/ivaquero/museek/cli"
+)
+
+// main the entrypoint for the downloader.
+func main() {
+	cli.Execute()
+}
